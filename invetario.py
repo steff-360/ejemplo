45f029b9dd16a94f2ccde965a4a4458f3362e92e
steff-360 <steff.18random@gmail.com>
2026-03-10 20:54:08 -0600

    Actualizando inventario

--- invetario.py
+++ invetario.py
@@ -1,8 +1,11 @@
 def separador():
-    print("+*30")
+    print("*"*30)
+
+
 def mostrar_opcion():
-    opc=int(input("Ingrese la opcion que desea hacer: "))
+    opc = int(input("Ingrese la opcion que desea hacer: "))
     print(opc)
+
 
 def agregar_producto(inventario):
     nombre = input("Nombre del producto: ")
@@ -14,6 +17,8 @@ def agregar_producto(inventario):
         "precio": precio,
         "cantidad": cantidad
     })
+
+
 def mostrar_productos(inventario):
     for producto in inventario:
         print(f"Nombre: {producto['nombre']}, Precio: {producto['precio']}, Cantidad: {producto['cantidad']}")
