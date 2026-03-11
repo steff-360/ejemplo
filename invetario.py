c36fbd1417c3fa48f78e91e4e2516fb37e82f087
steff-360 <steff.18random@gmail.com>
2026-03-10 20:45:53 -0600

    Actualizando proyecto

--- invetario.py
+++ invetario.py
@@ -4,7 +4,16 @@ def mostrar_opcion():
     opc=int(input("Ingrese la opcion que desea hacer: "))
     print(opc)
 
-def agregar_prodcutos():
-    pass
-def mmostrar_productos():
-    pass
+def agregar_producto(inventario):
+    nombre = input("Nombre del producto: ")
+    precio = float(input("Precio: "))
+    cantidad = int(input("Cantidad: "))
+
+    inventario.append({
+        "nombre": nombre,
+        "precio": precio,
+        "cantidad": cantidad
+    })
+def mostrar_productos(inventario):
+    for producto in inventario:
+        print(f"Nombre: {producto['nombre']}, Precio: {producto['precio']}, Cantidad: {producto['cantidad']}")
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-productos= {
+productos= {  
 
 }
+def listar_productos(inventario):
+    for producto in inventario:
+        print(producto)
+
+
 
 print=("Inventario de la Tienda Lola")
